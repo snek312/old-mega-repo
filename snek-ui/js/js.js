d1b8619cc70322fa034ea6cb1940c5544ba9f525
@bartosz.lazarski <b4rt99@gmail.com>
2018-11-11 08:23:49 +0100

    0.8c

--- snek-ui/js/js.js
+++ snek-ui/js/js.js
@@ -2,6 +2,7 @@
     window.addEventListener("scroll", function () {
         const elementNav = document.getElementById('id-nav').classList;
         const logoNav = document.getElementById('id-logo_text').classList;
+        const upButton = document.getElementById('id-up_button').classList;
         let elements = document.getElementsByClassName('ui-list--item');
 
         logoNav.add('logo-hover-black');
@@ -18,7 +19,14 @@
                 elements[i].classList.remove('ui-nav-button');
             }
         }
+
+        if (this.scrollY > 300) {
+            upButton.add('doc-up_active');
+        } else if (this.scrollY < 300) {
+            upButton.remove('doc-up_active');
+        }
     })
+
 
     document.getElementById('id-dropdown').addEventListener("click", function () {
         const element = document.getElementById('id-dropdown_list').classList;
@@ -33,4 +41,14 @@
         }
 
     })
+
+    const elementDrop = document.getElementById('id-dropdown_list').classList;
+    const elementIcon = document.getElementById('id-drop_icon').classList;
+
+    if (elementDrop.contains('dropdown--active')) {
+        document.addEventListener("click", function () {
+            elementDrop.remove('dropdown--active');
+            elementIcon.remove('dropdown--icon-active');
+        })
+    }
 })()
