f9aa0cf1ec68584bd219961d7ca90d04f030271c
@bartosz.lazarski <b4rt99@gmail.com>
2018-12-18 07:35:58 +0100

    0.1.0b

--- snek-ui/src/scripts/pages-docs/database.js
+++ snek-ui/src/scripts/pages-docs/database.js
@@ -1,4 +1,4 @@
-const version = '0.1.0';
+const version = '0.1.0b';
 
 const startDB = [{
         id: "header",
@@ -7,35 +7,128 @@ const startDB = [{
     },
     {
         id: "article",
-        header: "Article header",
-        subtext: "Subheader for more info",
+        header: "Introduce to Snek UI Docs",
+        subtext: "ECMA5 based",
         content: [
-            'lorem lorem w dupe worem',
-            'witam2',
-            'zium',
+            "This will be simple example of hyperlink in a paragraph <a href='button.html'>test link</a>",
+            'This documentation page will provide any info that you need know to use Snek UI',
             {
                 id: "message",
-                content: 'First example message.'
+                content: 'Current page is based on ECMA5 therefore syntax may be outdated.'
             }
         ]
     },
     {
         id: "article",
-        header: "Another article with diffrent data",
+        header: "How to use Snek Docs ",
+        subtext: "Navigation menu",
+
         content: [
-            'lorem lorem w dupe worem',
-            'Two examples bellow.',
+            'On the left side of page you will find navigation menu',
             {
                 id: "message",
-                content: 'Second example message.'
+                content: '<p>On mobile click hamburger icon to open menu. <i class="fas fa-exclamation-circle"></i></p>'
+            },
+            'Menu divide on two major sections:',
+            '- Buttons (single item, more data)',
+            '- Arrays (multiple list items contains less but specific data about individual element)',
+            {
+                id: "img",
+                src: '../src/img/db_img/example-1.png'
             },
             {
                 id: "message",
-                content: 'Created another example message. I hope you liked it. Lorem lorem lorems'
+                content: '<p>Faded out items meaning not available content at this moment. <i class="fas fa-exclamation-circle"></i></p>'
+            },
+        ]
+    },
+    {
+        id: "article",
+        header: " ",
+        subtext: "Buttons",
+
+        content: [
+            'Buttons move you to bigger pages with more informations, focusing more on general issues than individual one.',
+            {
+                id: "message",
+                content: 'Message.'
+            },
+
+        ]
+    },
+    {
+        id: "article",
+        header: " ",
+        subtext: "Arrays",
+
+        content: [
+            'That is a list with every docs content, each list item have its own individual page with simple instructions how to create, code snippet and examples. ',
+        ]
+    },
+    {
+        id: "article",
+        header: "Adding stuff to pseudo database",
+        subtext: "Pseudo database",
+
+        content: [
+            "This will be simple example of hyperlink in a paragraph <a href='button.html'>test link</a>",
+            'Current version of Snek Docs is using pseudo JS database to show content on site. JSDB is a noSQL-like database which contains page content data stored in arrays and objects.',
+            'Each array represents diffrent section of data ',
+
+            {
+                id: "img",
+                src: '../src/img/db_img/example-2.png'
+            },
+            {
+                id: "img",
+                src: '../src/img/db_img/example-3.png'
+            }
+        ]
+    },
+    {
+        id: "article",
+        header: " ",
+        subtext: "Section arrays",
+
+        content: [
+            'Each section array contains objects. Page header object is always placed on 0 index of array. Next index numbers represent articles objects with its own properties. ',
+            'Article objects uses declared string key properties, they do what the name suggests, however there is a declared list with explanation for each individual one:',
+            {
+                id: "code",
+                content: '<p> id - object type name </p> <p>header - article header (only one per page and article) </p> <p> subtext - subheader with lighter color (only one per page and article)</p> <p>content - content of the object which is an array, key may be <strong>string</strong> (witch represent paragraph) or <strong>object</strong>. Each content object key have its own key properties: </p> <p> -- id - type of content object </p> <p> -- content - text ["string"]</p> <p> -- styles - css rules ["string"]</p> <p> -- src - source for file ["string"]</p>'
+            }
+
+        ]
+    },
+    {
+        id: "article",
+        header: "List of id types",
+        subtext: "Object names",
+
+        content: [
+            '- <strong>header</strong> - create page header {<em>id, header, subtext </em>}',
+            '- <strong>article</strong> - create new article {<em>id, header, subtext, content </em>}',
+
+        ]
+    },
+    {
+        id: "article",
+        header: " ",
+        subtext: "Object content id's",
+
+        content: [
+            '- <strong>example</strong> - create new button {<em> id, styles, content </em>}',
+            '- <strong>message</strong> - create new message {<em> id, content </em>}',
+            '- <strong>code</strong> - create new code snippet {<em> id, content </em>}',
+            '- <strong>warning</strong> - create new warning message {<em> id, content </em>}',
+            '- <strong>img</strong> - create new img {<em> id,  src </em>}',
+            {
+                id: 'message',
+                content: 'More stuff comming soon.'
             }
         ]
     }
-]
+];
 const newDB = [{
         id: "header",
         header: "What's new in " + version,
@@ -43,7 +136,30 @@ const newDB = [{
     },
     {
         id: "article",
-        header: "Changes log",
+        header: "Changes log 0.1.0b",
+        content: [
+            '- added mobile version of snek docs',
+            '- snek docs css cleaning',
+            '- js cleaning',
+            '- added image element creator',
+            '- added code snippet element creator',
+            '- database docs updated'
+        ],
+
+    },
+    {
+        id: "article",
+        header: "Changes log 0.1.0a",
+        content: [
+            '- added message element to ui',
+            '- button and message docs updated',
+            '- js cleaning'
+        ]
+    },
+    {
+        id: "article",
+        header: "Changes log 0.1.0",
+        subtext: "Alpha version",
         content: [
             '- created first version of Snek Docs',
             '- created pseudo database for Snek Docs',
@@ -51,23 +167,13 @@ const newDB = [{
             '- bug fixes',
             '- updated default theme',
             '- added new things to the library',
-
-
             {
                 id: "message",
                 content: 'Library is still in very early development stage, so please issue any bugs you find! '
             }
         ]
     },
-    {
-        id: "article",
-        header: "0.1.0a",
-        content: [
-            '- added message element to ui',
-            '- button and message docs updated',
-            '- js cleaning'
-        ]
-    },
+
     {
         id: "article",
         header: "Future plans",
@@ -83,7 +189,7 @@ const newDB = [{
             }
         ]
     }
-]
+];
 const buttonDB = [{
         id: "header",
         header: "Button",
@@ -99,7 +205,7 @@ const buttonDB = [{
                 content: 'basic'
             },
             {
-                id: "message",
+                id: "code",
                 content: '<p>&lt;button class="<strong>button-color ui-button</strong>"&gt;basic&lt;/button&gt;</p>'
             }
         ]
@@ -120,13 +226,13 @@ const buttonDB = [{
                 content: 'dark'
             },
             {
-                id: "message",
+                id: "code",
                 content: '<p> &lt;button class="<strong>button-color button-color--alt ui-button</strong>"&gt;alternative&lt;/button&gt; </p> <p> &lt;button class="<strong>button-color button-color--dark ui-button</strong>"&gt;alternative&lt;/button&gt;</p> ',
             }
 
         ]
     }
-]
+];
 
 const containerDB = [{
         id: "header",
@@ -139,12 +245,12 @@ const containerDB = [{
         content: [
             'You cant see it, but its here',
             {
-                id: "message",
-                content: 'Something.'
+                id: "code",
+                content: '<p> &lt;div class="<strong> </strong>"&gt; &lt;/div&gt; </p>',
             }
         ]
     }
-]
+];
 
 const messageDB = [{
         id: "header",
@@ -159,6 +265,10 @@ const messageDB = [{
             {
                 id: "message",
                 content: '<p>This is an example. <i class="fas fa-exclamation-circle"></i></p>'
+            },
+            {
+                id: "code",
+                content: '<p> &lt;div class="<strong>ui-message</strong>"&gt;This is an example&lt;/div&gt; </p>',
             }
         ]
     },
@@ -170,10 +280,29 @@ const messageDB = [{
             {
                 id: "warning",
                 content: '<p></i>Warning! Site not working properly. Contact somebody! <i class="fas fa-exclamation-triangle"></p>'
+            },
+            {
+                id: "code",
+                content: '<p> &lt;div class="<strong>ui-message message--warning</strong>"&gt;This is an example&lt;/div&gt; </p>',
+            }
+        ]
+    },
+    {
+        id: "article",
+        header: "Code",
+        content: [
+            'Code snippet for sharing',
+            {
+                id: "code",
+                content: '<p>console.log("example");</p>'
+            },
+            {
+                id: "code",
+                content: '<p> &lt;div class="<strong>ui-message message--code</strong>"&gt;<p>console.log("example")</p>&lt;/div&gt; </p>',
             }
         ]
     }
-]
+];
 
 const database = [{
         id: "start",
@@ -197,8 +326,8 @@ const database = [{
     }
 ];
 
-const buttonsArray = ['start', 'new', ]
-const howtouseArray = ['buttojn', 'buttodwa', 'dwa', 'ziu']
+const buttonsArray = ['start', 'new'];
+const howtouseArray = ['structure', 'naming', 'themes', 'download'];
 const elementsArray = ['button', 'container', 'message', 'divider'];
 const modulesArray = ['dropdown'];
 
--- snek-ui/src/scripts/pages-docs/script.js
+++ snek-ui/src/scripts/pages-docs/script.js
@@ -1,8 +1,82 @@
 (function () {
+    // MAIN CODE
 
+    createMobileNavbar();
     navmenuElementsCreator();
     checkTitle();
 
+    // --
+    // FUNCTIONS 
+
+    // MODULE 1 - Mobile navbar
+    function createMobileNavbar() {
+        const mediaQuery = window.innerWidth;
+
+        if (mediaQuery < 769) {
+            const divElement = document.createElement('div');
+            const groupElement = document.createElement('div');
+
+            divElement.innerHTML = '<i class="fas fa-bars"></i>';
+            groupElement.appendChild(divElement);
+            groupElement.classList.add('doc-mobile-navbar');
+            const navbarElement = document.querySelector('.doc-navbar');
+
+            document.getElementsByTagName('nav')[0].insertBefore(groupElement, navbarElement);
+            document.querySelector('.doc-mobile-navbar').addEventListener('click', function () {
+                const e = navbarElement.classList;
+                if (e.contains('activated')) {
+                    e.remove('activated');
+                } else {
+                    e.add('activated');
+                }
+            })
+        }
+    }
+    // --
+    // MODULE2 - navbar menu elements
+    function navmenuElementsCreator() {
+        elementInit('id-howtouse', howtouseArray);
+        elementInit('id-elements', elementsArray);
+        elementInit('id-modules', modulesArray);
+        document.getElementById('id-version').innerHTML = 'new in ' + version;
+    }
+
+    function elementInit(id, array) {
+        for (let i = 0; i < array.length; i++) {
+            MenuElementCreator(id, array, i);
+        }
+    }
+
+    function MenuElementCreator(id, array, i) {
+        const listItemElement = document.createElement('li');
+        const anchorElement = document.createElement('a');
+        const divElement = document.createElement('div');
+
+        divElement.innerHTML = array[i];
+        checkExist(anchorElement, divElement, array, i);
+
+        anchorElement.appendChild(divElement);
+        listItemElement.appendChild(anchorElement);
+
+        document.getElementById(id).appendChild(listItemElement);
+    }
+
+    function checkExist(element, childElement, array, i) {
+
+        for (let j = 0; j < database.length; j++) {
+            const check = array[i].includes(database[j].id);
+
+            if (check) {
+                childElement.classList.remove('doc-no_click');
+                childElement.classList.add('doc-nav_li');
+                element.setAttribute('href', array[i] + '.html');
+            } else if (!childElement.classList.contains('doc-nav_li')) {
+                childElement.classList.add('doc-no_click');
+            }
+        }
+    }
+    // --
+    // MODULE 3 - page content
     function checkTitle() {
         const title = document.title;
         for (let i = 0; i < menuDB.length; i++) {
@@ -46,11 +120,18 @@
         }
     }
 
-    function navmenuElementsCreator() {
-        elementInit('id-howtouse', howtouseArray);
-        elementInit('id-elements', elementsArray);
-        elementInit('id-modules', modulesArray);
-        document.getElementById('id-version').innerHTML = 'new in ' + version;
+    function MainHeaderCreator(id, database) {
+        const h1Element = document.createElement('h1');
+        const h3Element = document.createElement('h3');
+        const divElement = document.createElement('div');
+
+        h1Element.innerHTML = database[0].header;
+        h3Element.innerHTML = database[0].subtext;
+
+        divElement.appendChild(h1Element);
+        divElement.appendChild(h3Element);
+
+        document.getElementById(id).appendChild(divElement);
     }
 
     function ArticleCreator(id, database, i) {
@@ -58,14 +139,29 @@
         articleElement.classList.add('doc-article');
 
         HeaderCreator(articleElement, database, i);
-        ParagraphCreator(articleElement, database, i);
+        ArticleContentCreator(articleElement, database, i);
 
         document.getElementById(id).appendChild(articleElement);
-
-
     }
 
-    function ParagraphCreator(element, database, i) {
+    function HeaderCreator(element, database, i) {
+        const h2Element = document.createElement('h2');
+        const headerElement = document.createElement('header');
+
+        h2Element.innerHTML = database[i].header;
+        headerElement.classList.add('doc-article_header');
+        headerElement.appendChild(h2Element);
+
+        if (database[i].subtext) {
+            const h4Element = document.createElement('h4');
+            h4Element.innerHTML = database[i].subtext;
+            headerElement.appendChild(h4Element);
+        }
+
+        element.appendChild(headerElement);
+    }
+
+    function ArticleContentCreator(element, database, i) {
         const groupElement = document.createElement('div');
         groupElement.classList.add('flex-box', 'flex-column');
         for (j = 0; j < database[i].content.length; j++) {
@@ -82,6 +178,12 @@
                     break;
                 case database[i].content[j].id === 'warning':
                     WarningElementCreator(element, database, i, j);
+                    break;
+                case database[i].content[j].id === 'code':
+                    CodeElementCreator(element, database, i, j);
+                    break;
+                case database[i].content[j].id === 'img':
+                    ImgElementCreator(element, database, i, j);
                     break;
             }
         }
@@ -126,70 +228,20 @@
         element.appendChild(divElement);
     }
 
-    function elementInit(id, array) {
-        for (let i = 0; i < array.length; i++) {
-            MenuElementCreator(id, array, i);
-        }
-    }
-
-    function checkExist(element, childElement, array, i) {
-
-        for (let j = 0; j < database.length; j++) {
-            const check = array[i].includes(database[j].id);
-
-            if (check) {
-                childElement.classList.remove('doc-no_click');
-                childElement.classList.add('doc-nav_li');
-                element.setAttribute('href', array[i] + '.html');
-            } else if (!childElement.classList.contains('doc-nav_li')) {
-                childElement.classList.add('doc-no_click');
-            }
-        }
-    }
-
-    function MainHeaderCreator(id, database) {
-        const h1Element = document.createElement('h1');
-        const h3Element = document.createElement('h3');
+    function CodeElementCreator(element, database, i, j) {
         const divElement = document.createElement('div');
-
-        h1Element.innerHTML = database[0].header;
-        h3Element.innerHTML = database[0].subtext;
-
-        divElement.appendChild(h1Element);
-        divElement.appendChild(h3Element);
-
-        document.getElementById(id).appendChild(divElement);
+        divElement.classList.add('ui-message', 'message--code');
+        divElement.innerHTML = database[i].content[j].content;
+        element.appendChild(divElement);
     }
 
-    function HeaderCreator(element, database, i) {
-        const h2Element = document.createElement('h2');
-        const headerElement = document.createElement('header');
+    function ImgElementCreator(element, database, i, j) {
+        const imgElement = document.createElement('img');
+        // imgElement.innerHTML = database[i].content[j].src;
+        imgElement.setAttribute('src', database[i].content[j].src);
 
-        h2Element.innerHTML = database[i].header;
-        headerElement.classList.add('doc-article_header');
-        headerElement.appendChild(h2Element);
-
-        if (database[i].subtext) {
-            const h4Element = document.createElement('h4');
-            h4Element.innerHTML = database[i].subtext;
-            headerElement.appendChild(h4Element);
-        }
-
-        element.appendChild(headerElement);
-
+        element.appendChild(imgElement);
     }
 
-    function MenuElementCreator(id, array, i) {
-        const listItemElement = document.createElement('li');
-        const anchorElement = document.createElement('a');
-        const divElement = document.createElement('div');
 
-        divElement.innerHTML = array[i];
-        checkExist(anchorElement, divElement, array, i);
-
-        anchorElement.appendChild(divElement);
-        listItemElement.appendChild(anchorElement);
-
-        document.getElementById(id).appendChild(listItemElement);
-    }
 }())
