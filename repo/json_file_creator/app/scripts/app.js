c7ebb9cf5afcc885c42d3bf5e1b9128bec475cfb
Ziu <you@example.com>
2019-03-02 03:49:49 +0100

    update1

--- repo/json_file_creator/app/scripts/app.js
+++ repo/json_file_creator/app/scripts/app.js
@@ -1,42 +1,35 @@
-let value = [];
-const obj = {};
-let key, values;
-
-document.getElementById('id-newMotyw').addEventListener('click', () => {
+document.getElementById('id-newItem').addEventListener('click', () => {
     const
-        motyw = document.getElementById('id-newMotyw'),
+        motyw = document.getElementById('id-newItem'),
         parentMotyw = motyw.parentNode,
         div = document.createElement('div'),
         divDwa = document.createElement('div'),
         button = document.createElement('button');
 
-
     button.setAttribute('class', 'button');
-    button.innerText = '+';
+    button.innerHTML = '<i class="fas fa-plus">';
     divDwa.setAttribute('class', 'parent');
-
     divDwa.appendChild(button);
 
-    elementListener(button, divDwa, button);
+    dropdownBtnListener(button, divDwa, button);
+
     div.setAttribute('class', 'firstWrapper');
-
-
     div.appendChild(divDwa)
     parentMotyw.insertBefore(div, motyw);
-    // testbozy()
-
 })
 
-function elementListener(element, parent, before) {
+////////////////////////
+//// Listeners functions
+////////////////////////
+
+function dropdownBtnListener(element, parent, before) {
     return element.addEventListener('click', () => {
         const dropdownElement = document.querySelector('.dropdown');
 
         if (dropdownElement) {
             dropdownElement.remove();
         } else {
-            // create(parent, before);
             dropdown(element, before);
-            // parent.remove(element);
             document.getElementById('op1').addEventListener('click', () => {
                 const node = parent.querySelector('.nodeName');
                 if (node) {
@@ -44,8 +37,6 @@ function elementListener(element, parent, before) {
                 } else {
                     createNode(parent, before, 'nodeName');
                 }
-                // createNode(parent, before, 'nodeName');
-                // document.querySelector('.dropdown').remove();
             });
             document.getElementById('op2').addEventListener('click', () => {
                 const node = parent.querySelector('.attribute');
@@ -56,7 +47,7 @@ function elementListener(element, parent, before) {
                 }
             });
             document.getElementById('op3').addEventListener('click', () => {
-                testbozy(parent)
+                createChild(parent)
             });
             document.getElementById('op4').addEventListener('click', () => {
                 const node = parent.querySelector('.textContent');
@@ -65,72 +56,78 @@ function elementListener(element, parent, before) {
                 } else {
                     createNode(parent, before, 'textContent');
                     testListener(parent, before, 'textContent');
-
                 }
             })
         }
-
     })
 }
 
-
-function testListener(element, parent, before) {
-    const downloadButton = document.getElementById('id-download');
-    return downloadButton.addEventListener('click', () => {
-
-        console.log([parent.parentNode]);
-        console.log(obj);
-
-        if (parent.parentNode.classList.contains('parent')) {
-            const tescik = element.querySelector('.textContent').value;
-            key = 'textContent';
-            value = tescik;
-            obj[key] = value;
-            console.log('tak')
-        } else {
-            const tescik = element.querySelector('.textContent').value;
-
-            if (obj.children) {
-                chl = 'children'
-                key = key + [chl];
-                value = {
-                    textContent: tescik
-                };
-                obj[key] = value;
-            } else {
-                key = 'children';
-                value = {
-                    textContent: tescik
-                };
-                obj[key] = value;
-            }
-
-            console.log('tak children')
-        }
-
-        console.log(obj);
-
-    })
-}
-
-function buttonListener(element, parent, before) {
+function AttrBtnListener(element, parent, before) {
     return element.addEventListener('click', () => {
-        createtest(parent, before)
+        createAttributeKey(parent, before)
     })
 }
+
+/////////////////////
+//// Create Functions
+/////////////////////
 
 function create(parent, before) {
     const
         element = document.createElement('span');
-
     element.innerText = 'witam';
     parent.insertBefore(element, before);
+}
+/////////////////////
+/////////////////////
 
+function createNode(parent, before, spanText) {
+    const
+        element = document.createElement('div'),
+        tpl = `<span class="${spanText}">${spanText}</span>
+        <input type="text" placeholder="name">`
+    element.insertAdjacentHTML("afterbegin", tpl)
+
+    parent.insertBefore(element, before);
+}
+
+function createAttr(parent, before) {
+    const
+        element = document.createElement('div'),
+        div = document.createElement('div'),
+        tpl = `<span>attributes</span>
+        <div>
+            <input type="text" placeholder="key">
+            <input type="text" placeholder="value">
+        </div>`,
+        button = document.createElement('button');
+
+    element.setAttribute('class', 'attribute');
+    button.setAttribute('class', 'button');
+    button.innerText = '+';
+
+    AttrBtnListener(button, element, button);
+
+    element.insertAdjacentHTML("afterbegin", tpl)
+    element.appendChild(div);
+    element.appendChild(button);
+
+    parent.insertBefore(element, before);
+}
+
+function createAttributeKey(parent, before) {
+    const
+        element = document.createElement('div'),
+        tpl = `<input type="text" placeholder="key">
+        <input type="text" placeholder="value">`;
+
+    element.insertAdjacentHTML("afterbegin", tpl)
+
+    parent.insertBefore(element, before);
 }
 
 function createError(text = '') {
     const
-
         element = document.createElement('div'),
         span = document.createElement('span');
 
@@ -146,88 +143,6 @@ function createError(text = '') {
     interval;
 }
 
-
-function createtest(parent, before) {
-    const
-        element = document.createElement('div'),
-        inputKey = document.createElement('input'),
-        inputValue = document.createElement('input');
-
-    inputKey.setAttribute('type', 'text');
-    inputKey.setAttribute('placeholder', 'key');
-
-    inputValue.setAttribute('type', 'text');
-    inputValue.setAttribute('placeholder', 'value');
-
-    element.appendChild(inputKey);
-    element.appendChild(inputValue);
-
-    parent.insertBefore(element, before);
-
-}
-
-
-function createNode(parent, before, spanText) {
-    const
-        element = document.createElement('div'),
-        span = document.createElement('span'),
-        input = document.createElement('input');
-
-    input.setAttribute('class', spanText);
-    input.setAttribute('type', 'text');
-    input.setAttribute('placeholder', 'name');
-
-
-
-    span.innerText = spanText;
-    element.appendChild(span);
-    element.appendChild(input);
-
-    parent.insertBefore(element, before);
-
-}
-
-function createAttr(parent, before) {
-    const
-        element = document.createElement('div'),
-        div = document.createElement('div'),
-
-        span = document.createElement('span'),
-        inputKey = document.createElement('input'),
-        inputValue = document.createElement('input'),
-        button = document.createElement('button');
-
-
-    element.setAttribute('class', 'attribute');
-
-    inputKey.setAttribute('type', 'text');
-    inputKey.setAttribute('placeholder', 'key');
-
-    inputValue.setAttribute('type', 'text');
-    inputValue.setAttribute('placeholder', 'value');
-
-    button.setAttribute('class', 'button');
-    button.innerText = '+';
-
-    buttonListener(button, element, button);
-
-
-
-    span.innerText = 'attributes';
-    div.appendChild(inputKey);
-    div.appendChild(inputValue);
-
-    element.appendChild(span);
-    element.appendChild(div);
-    element.appendChild(button);
-
-
-
-    parent.insertBefore(element, before);
-
-
-}
-
 function dropdown(parent) {
     const
         div = document.createElement('div'),
@@ -237,112 +152,23 @@ function dropdown(parent) {
         <button id="op4" class="option">Text</button>`;
 
     div.setAttribute('class', 'dropdown active');
-    div.innerHTML = tpl;
+    div.insertAdjacentHTML("afterbegin", tpl)
     parent.appendChild(div);
-
 }
 
-
-function testbozy(parent) {
+function createChild(parent) {
     const
         parentMotyw = parent,
         div = document.createElement('div'),
         divDwa = document.createElement('div'),
         button = document.createElement('button');
 
-
     button.setAttribute('class', 'button');
     button.innerText = '+';
     divDwa.appendChild(button);
+    dropdownBtnListener(button, divDwa, button);
 
-    elementListener(button, divDwa, button);
     div.setAttribute('class', 'firstWrapper');
-
-
     div.appendChild(divDwa);
     parentMotyw.appendChild(div);
-
 }
-////////////////////////////
-
-// document.getElementById('id-download').addEventListener('click', () => {
-
-
-//     console.log('test')
-
-
-
-
-
-//     // const test = dupa('.nodeName');
-//     // const test2 = dupa('.textContent');
-
-//     // console.log(test);
-//     // let value = [];
-
-//     // for (let i = 0; i < test.length; i++) {
-
-//     //     dupatestowa(test, test2, i, value);
-//     // }
-//     // console.log(JSON.stringify(value));
-
-// })
-
-
-
-function dupa(ziu = ' ') {
-    const test = document.querySelectorAll(ziu)
-
-    return test;
-}
-
-
-function dupatestowa(ddd = [], dwa = [], i, value) {
-    const obj = {};
-    let key, values;
-    const ziu = ddd[i].value;
-    const ziu2 = dwa[i].value;
-
-
-    ppp(obj, key, 'nodeNames', values, ziu);
-    ppp(obj, key, 'textContent', values, ziu2);
-
-
-
-    console.log(ziu);
-    console.log(obj)
-    value.push(obj);
-
-}
-
-
-function ppp(obj, key, keyString, values, valuesD) {
-    key = keyString;
-    values = valuesD;
-    return obj[key] = values;
-}
-
-
-// let
-//     value = [];
-// // value[0].children = {};
-// console.log(value)
-// console.log(JSON.stringify(value));
-
-// // ////////////////////////////////////////
-// const obj = {}
-// let key = 'dupa';
-// let values = 'dupo'
-
-// obj[key] = values;
-// console.log(obj);
-
-// key = 'dupaa';
-// values = 'dupow'
-
-// obj[key] = values;
-
-// console.log(obj);
-// value.push(obj);
-// console.log(value);
-// console.log(JSON.stringify(value));
