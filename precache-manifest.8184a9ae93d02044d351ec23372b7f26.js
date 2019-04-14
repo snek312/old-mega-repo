d6944bc0d7ee8b4a295a3b64088e438edd03bcc6
@bartosz.lazarski <b4rt99@gmail.com>
2019-04-15 01:46:44 +0200

    Updates

---
+++ precache-manifest.8184a9ae93d02044d351ec23372b7f26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "2c4e18d13e8207a75fe7",
+    "url": "/myapp/static/js/main.c19014c4.chunk.js"
+  },
+  {
+    "revision": "1572e52a2e6d24ac799c",
+    "url": "/myapp/static/js/2.8a370f54.chunk.js"
+  },
+  {
+    "revision": "2c4e18d13e8207a75fe7",
+    "url": "/myapp/static/css/main.7da23d7a.chunk.css"
+  },
+  {
+    "revision": "ff83bd37b3e43a94c2af89de4c472e82",
+    "url": "/myapp/index.html"
+  }
+];
