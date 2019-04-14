fb397e53ddec6df3a6b0c4002e86f91f20ac793c
@bartosz.lazarski <b4rt99@gmail.com>
2019-04-15 01:07:00 +0200

    test

---
+++ myapp/precache-manifest.5d50d1013d2f65b27e2c1db80d483fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "ddc1bce2a22c93836459",
+    "url": "/myapp/static/js/main.8909382f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/myapp/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ddc1bce2a22c93836459",
+    "url": "/myapp/static/css/main.7da23d7a.chunk.css"
+  },
+  {
+    "revision": "7f70d33f851e225ce5e64e26e44fb146",
+    "url": "/myapp/index.html"
+  }
+];
