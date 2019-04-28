174baac3d27f3744036c3f9c376d354c14b185bb
@bartosz.lazarski <b4rt99@gmail.com>
2019-04-29 00:25:15 +0200

    react-init

---
+++ fed-wo1/precache-manifest.d451355c1f063e79a0bc18d821df8e7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3102f3c22ac2a8102dde048585ade366",
+    "url": "/fed-wo1/index.html"
+  },
+  {
+    "revision": "6c1dbda89f1b4c5b9867",
+    "url": "/fed-wo1/static/css/main.b4665f5a.chunk.css"
+  },
+  {
+    "revision": "be5829938a690f7a75d1",
+    "url": "/fed-wo1/static/js/2.b4625e90.chunk.js"
+  },
+  {
+    "revision": "6c1dbda89f1b4c5b9867",
+    "url": "/fed-wo1/static/js/main.79f02e8f.chunk.js"
+  },
+  {
+    "revision": "5abe408d35fd6d92f4eb",
+    "url": "/fed-wo1/static/js/runtime~main.06b56ae3.js"
+  },
+  {
+    "revision": "f8f342449f8d603ced541a3b75e0b13e",
+    "url": "/fed-wo1/static/media/autko.f8f34244.png"
+  },
+  {
+    "revision": "ab8768a95f76b061cd728944405228ec",
+    "url": "/fed-wo1/static/media/img.ab8768a9.jpg"
+  },
+  {
+    "revision": "8a3ea42b8221ea40e5fbbd7928e57012",
+    "url": "/fed-wo1/static/media/logo1.8a3ea42b.png"
+  }
+]);
