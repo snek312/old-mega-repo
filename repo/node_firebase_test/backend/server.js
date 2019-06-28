5b2116ed4858acf59effca108e778988ebabb9a0
@bartosz.lazarski <b4rt99@gmail.com>
2019-06-28 23:26:25 +0200

    ddd

--- repo/node_firebase_test/backend/server.js
+++ repo/node_firebase_test/backend/server.js
@@ -6,17 +6,27 @@ const port = 5000;
 app.use(express.json()) // for parsing application/json
 app.use(cors())
 
-const fakeDB = {
-    name: {
-        test: 'test',
-        test2: 'test2'
-    }
-}
+
+//Firebase App constans
+
+const firebaseApp = require('./config/firebase');
+const database = firebaseApp.database();
+const refData = database.ref();
+
+// const fakeDB = {
+//     name: {
+//         test: 'test',
+//         test2: 'test2'
+//     }
+// }
 
 
 app.get('/api', (req, res) => {
     console.log('Witam w api');
-    res.json(fakeDB)
+    refData().once('value', snap => {
+        const data = JSON.stringify(snap.val())
+        res.json(data)
+    })
 })
 
 app.post('/api', (req, res) => {
